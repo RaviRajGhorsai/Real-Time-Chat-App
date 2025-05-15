93a2b58d08560608d0dd17c63ff31e640c95310b
RaviRajGhorsai <graviraj344@gmail.com>
2025-05-15 23:20:53 +0545

    final 2

--- Project/asgi.py
+++ Project/asgi.py
@@ -8,12 +8,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application 
 from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 from channels.auth import AuthMiddlewareStack
 from app import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),

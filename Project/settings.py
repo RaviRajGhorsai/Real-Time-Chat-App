9985ce4008646697364bf22f15016e55c3bad343
RaviRajGhorsai <graviraj344@gmail.com>
2025-05-16 13:53:48 +0545

    media

--- Project/settings.py
+++ Project/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import environ
- 
+import cloudinary
+from django.conf import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- app/apps.py
+++ app/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
-
+import cloudinary
+from django.conf import settings
 
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -7,3 +8,10 @@ class AppConfig(AppConfig):
 
     def ready(self):
         import app.signals
+
+        cloudinary.config(
+            cloud_name = settings.CLOUDINARY_STORAGE['CLOUD_NAME'],
+            api_key = settings.CLOUDINARY_STORAGE['API_KEY'],
+            api_secret = settings.CLOUDINARY_STORAGE['API_SECRET']
+        )
+    
